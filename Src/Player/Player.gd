bb103d10f04c5311b3430b5188344fd82e3c9044
MilliesXu <winzchip@gmail.com>
2022-10-30 17:16:09 +0700

    Add collission

--- Src/Player/Player.gd
+++ Src/Player/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 100
-const ACCELERATION = 400
-const FRICTION = 400
+const MAX_SPEED = 80
+const ACCELERATION = 500
+const FRICTION = 500
 
 var velocity: = Vector2.ZERO
 
@@ -18,15 +18,10 @@ func _process(delta: float) -> void:
 	input_vector = input_vector.normalized()
 	
 	# Make sure the character stop when not pressing anything
-	if input_vector != Vector2.ZERO:
-		# velocity = input_vector * MAX_SPEED
-		
-		# Using acceleration example
-		velocity += input_vector * ACCELERATION * delta
+	if input_vector != Vector2.ZERO:	
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		# velocity = Vector2.ZERO
-		
 		# Applying friction to slow down when not pressing anything
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	move_and_collide(velocity * delta)
+	velocity = move_and_slide(velocity)
